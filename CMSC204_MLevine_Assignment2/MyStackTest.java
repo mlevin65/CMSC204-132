bfb2375ee8393837325c60db79b22ef5ceb2399b
mlevin65 <93367153+mlevin65@users.noreply.github.com>
2022-10-02 21:34:14 -0400

    Add files via upload

---
+++ CMSC204_MLevine_Assignment2/MyStackTest.java
@@ -0,0 +1,210 @@
+/*
+ * Class: CMSC204 
+ * Instructor: Prof. Kuijt
+ * Description: You will be creating a utility class that converts an infix 
+ * expression to a postfix expression, a postfix expression to an infix 
+ * expression and evaluates a postfix expression.
+ * Due: 10/2/2022
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming assignment independently.
+   I have not copied the code from a student or any source.
+   I have not given my code to any student.
+   Print your Name here: Miles Levine
+*/
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyStackTest {
+	public MyStack<String> stringS;
+	public String a="a", b="b", c="c", d="d", e="e", f="f";
+	public ArrayList<String> fill = new ArrayList<String>();
+	
+	// STUDENT: student tests will use the doubleS
+	public MyStack<Double> doubleS;
+	// STUDENT: add variables as needed for your student tests
+	public Double g = 2.0, h = 4.0, i = 6.0, j = 8.0, k = 10.0, l = 12.0, m = 14.0;
+	
+	@Before
+	public void setUp() throws Exception {
+		stringS = new MyStack<String>(5);
+		stringS.push(a);
+		stringS.push(b);
+		stringS.push(c);
+		
+		//STUDENT: add setup for doubleS for student tests
+		doubleS = new MyStack<Double>(5);
+		doubleS.push(g);
+		doubleS.push(h);
+		doubleS.push(i);
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		stringS = null;
+		doubleS = null;
+	}
+
+	@Test
+	public void testIsEmpty() throws StackUnderflowException {
+		assertEquals(false,stringS.isEmpty());
+		stringS.pop();
+		stringS.pop();
+		stringS.pop();
+		assertEquals(true, stringS.isEmpty());
+	}
+
+	@Test
+	public void testIsFull() throws StackOverflowException {
+		assertEquals(false, stringS.isFull());
+		stringS.push(d);
+		stringS.push(e);
+		assertEquals(true, stringS.isFull());
+	}
+
+	@Test
+	public void testPop() {
+		try {
+			assertEquals(c, stringS.pop());
+			assertEquals(b, stringS.pop());
+			assertEquals(a, stringS.pop());
+			//Queue is empty, next statement should cause QueueUnderFlowException
+			stringS.pop();
+			assertTrue("This should have caused an StackUnderflowException", false);
+		}
+		catch (StackUnderflowException e){
+			assertTrue("This should have caused an StackUnderflowException", true);
+		}
+		catch (Exception e){
+			assertTrue("This should have caused an StackUnderflowException", false);
+		}
+	}
+
+	@Test
+	//fix
+	public void testPopStudent() {
+		try {
+			assertEquals(i, doubleS.pop());
+			assertEquals(h, doubleS.pop());
+			assertEquals(g, doubleS.pop());
+			doubleS.pop();
+			assertTrue("This should have caused an StackUnderflowException", false);
+		} catch (StackUnderflowException sue) {
+			assertTrue("This should have caused an StackUnderflowException", true);
+		} catch (Exception e) {
+			assertTrue("This should have caused an StackUnderflowException", false);
+		}
+
+	}
+	
+	@Test
+	public void testTop() throws StackUnderflowException, StackOverflowException {
+		assertEquals(c, stringS.top());
+		stringS.push(d);
+		assertEquals(d, stringS.top());
+		stringS.pop();
+		stringS.pop();
+		assertEquals(b, stringS.top());		
+	}
+
+	@Test
+	public void testSize() throws StackOverflowException, StackUnderflowException {
+		assertEquals(3, stringS.size());
+		stringS.push(d);
+		assertEquals(4, stringS.size());
+		stringS.pop();
+		stringS.pop();
+		assertEquals(2, stringS.size());
+	}
+
+	@Test
+	public void testPush() {
+		try {
+			assertEquals(3, stringS.size());
+			assertEquals(true, stringS.push(d));
+			assertEquals(4, stringS.size());
+			assertEquals(true, stringS.push(e));
+			assertEquals(5, stringS.size());
+			//Queue is full, next statement should cause QueueOverFlowException
+			stringS.push(f);
+			assertTrue("This should have caused an StackOverflowException", false);
+		}
+		catch (StackOverflowException e){
+			assertTrue("This should have caused an StackOverflowException", true);
+		}
+		catch (Exception e){
+			assertTrue("This should have caused an StackOverflowException", false);
+		}
+	}
+
+	@Test
+	//fix
+	public void testPushStudent() {
+		try {
+			assertEquals(3, doubleS.size());
+			assertEquals(true, doubleS.push(j));
+			assertEquals(4, doubleS.size());
+			assertEquals(true, doubleS.push(k));
+			assertEquals(5, doubleS.size());
+			// Stack is full, next statement should cause QueueOverFlowException
+			doubleS.push(l);
+			assertTrue("This should have caused an StackOverflowException", false);
+		} catch (StackOverflowException e) {
+			assertTrue("This should have caused an StackOverflowException", true);
+		} catch (Exception e) {
+			assertTrue("This should have caused an StackOverflowException", false);
+		}
+
+	}
+	
+	@Test
+	public void testToString() throws StackOverflowException {
+		assertEquals("abc", stringS.toString());
+		stringS.push(d);
+		assertEquals("abcd", stringS.toString());
+		stringS.push(e);
+		assertEquals("abcde", stringS.toString());
+	}
+
+	@Test
+	//fix
+	public void testToStringStudent() throws StackOverflowException {
+		assertEquals("2.04.06.0", doubleS.toString());
+		doubleS.push(j);
+		assertEquals("2.04.06.08.0", doubleS.toString());
+		doubleS.push(k);
+		assertEquals("2.04.06.08.010.0", doubleS.toString());
+
+	}
+	
+	@Test
+	public void testToStringDelimiter() throws StackOverflowException {
+		assertEquals("a%b%c", stringS.toString("%"));
+		stringS.push(d);
+		assertEquals("a&b&c&d", stringS.toString("&"));
+		stringS.push(e);
+		assertEquals("a/b/c/d/e", stringS.toString("/"));
+	}
+
+	@Test
+	public void testFill() throws StackUnderflowException {
+		fill.add("apple");
+		fill.add("banana");
+		fill.add("carrot");
+		//start with an empty queue
+		stringS = new MyStack<String>(5);
+		//fill with an ArrayList
+		stringS.fill(fill);
+		assertEquals(3,stringS.size());
+		assertEquals("carrot", stringS.pop());
+		assertEquals("banana", stringS.pop());
+		assertEquals("apple", stringS.pop());		
+	}
+
+}
