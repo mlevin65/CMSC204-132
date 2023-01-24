6bee3a2ec99b3a88703b6e1e477a8420f3dee698
Miles Levine <93367153+mlevin65@users.noreply.github.com>
2023-01-24 00:46:32 -0500

    Add files via upload

---
+++ CMSC204_Assignment4/Comparable.java
@@ -0,0 +1,15 @@
+/*
+ * Class: CMSC204 
+ * Instructor: Prof. Kuijt
+ * Description: Write a program that creates a database of courses.  
+ * It will either read from a file of courses or allow the user to add one course at a time. 
+ * Due: 11/6/2022
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming assignment independently.
+   I have not copied the code from a student or any source.
+   I have not given my code to any student.
+   Print your Name here: Miles Levine
+*/
+public interface Comparable {
+	int compareTo(CourseDBElement element);
+}
